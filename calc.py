f685d5852ebf9f726b3487a976f34be4661c5872
GirishPMallya <girish.mallya@amagi.com>
2019-07-24 12:18:39 +0530

    To test the git functionality

---
+++ calc.py
@@ -0,0 +1,26 @@
+
+def add(x,y):
+	return x+y
+
+def sub(x,y):
+	return x-y
+
+def mul(x,y):
+	return x*y
+
+def div(x,y):
+	return int(x/y)
+
+
+def main():
+	"this is calculator app"
+	x=1
+	y=2
+	print(add(x,y))
+	print(sub(x,y))
+	print(mul(x,y))
+	print(div(x,y))
+
+
+
+main()
